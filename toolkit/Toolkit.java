3f65e72b19873c2b89be01fb03e4a5b987f5a2ed
pankajptl789 <pankajptl2552@gmail.com>
2025-11-10 16:10:40 +0530

    Create Toolkit class with init and isAvailable methods

---
+++ toolkit/Toolkit.java
@@ -0,0 +1,7 @@
+package com.github.the-pig-of-jungle.toolkit;
+
+// Empty dummy class
+public class Toolkit {
+    public static void init() {}
+    public static boolean isAvailable() { return false; }
+}
